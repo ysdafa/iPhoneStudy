32e44a109d6c7f3752603634591b2022d0346342
ysdafa <yansu84@gmail.com>
2015-11-28 10:32:17 +0900

    Init version

--- Helloworld/main.m
+++ Helloworld/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World Yan Su!");
     }
     return 0;
 }
