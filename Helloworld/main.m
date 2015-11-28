766263ed0aa41571737a6bfe3d35bcea76574556
肃严 <yansirs@sina.com>
2015-11-28 09:42:31 +0900

    Initial Commit

---
+++ Helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Helloworld
+//
+//  Created by yansu on 11/28/15.
+//  Copyright © 2015 yansu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
